f50867ead190b3c6aa37dda78f16787aa0ea545d
Parth Sanghavi <142639326+Parthsanghavi31@users.noreply.github.com>
2023-10-23 03:15:33 -0400

    Update BundleAdjustment.py

--- BundleAdjustment.py
+++ BundleAdjustment.py
@@ -1,10 +1,4 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Nov  2 16:02:24 2022
 
-@author: dushyant
-"""
 import numpy as np
 
 def Bundle_Error(initial_list, All_Image_Points, Vis_Mat, P_size, W_size, K):
